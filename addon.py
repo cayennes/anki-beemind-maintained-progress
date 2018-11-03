67802f1aa30ce0da64746cf9e007480380c325b1
Cayenne Geis <cayennes@gmail.com>
2018-11-03 11:51:32 -0400

    display maintained progress

--- addon.py
+++ addon.py
@@ -1,7 +1,12 @@
 from aqt import qt, mw, utils
 
+def get_maintained_progress():
+    return len(mw.col.findCards("-is:suspended -is:new -is:due -is:buried"))
+
 def update():
-    utils.showInfo("This addon does't do anything yet")
+    info_string = ("This addon doesn't connect to beeminder yet.\n\n"
+                   "Your maintained progress is %s") % get_maintained_progress()
+    utils.showInfo(info_string)
 
 update_action = qt.QAction("Update Beeminder", mw)
 update_action.triggered.connect(update)
